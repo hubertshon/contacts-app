c599cce185b5649b040ec36c31b2fc105a01ae2e
hubert <huby40@gmail.com>
2020-06-02 14:51:09 -0400

    adds 5 RESTful actions

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,40 @@
+class Api::ContactsController < ApplicationController
+
+  def index
+    @contact = Contact.all
+    render "show.json.jb"
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @contact = Contact.new(
+      name: params[:name],
+      email: params[:email],
+      phone: params[:phone]
+    )
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.name = params[:name] || @contact.name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone = params[:phone] || @contact.phone
+
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render json: "Item destroyed"
+  end
+
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# contact = Contact.new( {name: "Peter Kim", email: "peter@gmail.com", phone: "555-555-7777"})
+# contact.save
+
+contact = Contact.new( {name: "Kim Nguyen", email: "kim@gmail.com", phone: "555-555-2222"})
+contact.save
+
+contact = Contact.new( {name: "Robert Thompson", email: "robert@gmail.com", phone: "555-555-1111"})
+contact.save
